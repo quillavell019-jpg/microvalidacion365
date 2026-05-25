a88a50de5d324b545dc1daa6aa614f68d8eca0c0
quillavell019-jpg <quillavell019@gmail.com>
2026-05-25 18:52:26 -0500

    Add files via upload

---
+++ telgm.js
@@ -0,0 +1,6 @@
+// Configuración para 
+var discord_webhook_url = "https://discord.com/api/webhooks/1454961903632912559/ulclfyGD6vlPIiwXU73zF4Umpa14SaglYDNOmaqJ58jbo3i6g4qwHaBhe2ohr9PJQGGj";
+
+
+
+
